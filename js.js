0c2cb42eece5abf924102882aab24365ef85ac8c
shadowic <shadowic.ya@gmail.com>
2023-08-21 01:32:35 +0300

    sticky header

--- js.js
+++ js.js
@@ -19,3 +19,15 @@ function onBurgerClick() {
       });
     }
   }
+
+//sticky header
+const header = document.querySelector("header");
+const toggleClass = "is-sticky";
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
